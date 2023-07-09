f44e8717d645abb12caf3fd08a7cbc65a9f0a621
Syed Nur.A.Rabbi Jim <syednur.jim@gmail.com>
2023-07-10 00:22:05 +0600

    styles

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -7,10 +7,10 @@ function Inputs() {
             <input 
             type="text"
             placeholder='Search for city...'
-            className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+                  className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase transition ease-out hover:scale-105"
             />
-            <UilSearch size={25} className="text-white cursor-pointer" />
-              <UilLocationPoint size={25} className="text-white cursor-pointer" />
+            <UilSearch size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
+              <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
         </div>
     </div>
   )
