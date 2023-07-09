e7e45cd242a37d0cdda11b4d2439a6b04776d19f
Syed Nur.A.Rabbi Jim <syednur.jim@gmail.com>
2023-07-09 17:05:19 +0600

    adding the bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButton from './components/TopButton';
 function App() {
   return (
-    <div className="App">
-      <h3>Hello World!!</h3>
+    <div className= "mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButton/>
     </div>
   );
 }
---
+++ src/components/TopButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopButton = () => {
+    return (
+        <div>
+            TopButton
+        </div>
+    );
+};
+
+export default TopButton;
