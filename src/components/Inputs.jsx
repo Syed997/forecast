fdeecae89ee8bfd723250fc48b31cb0d68ff6e4e
s.zim <syednur.jim@gmail.com>
2023-07-12 14:29:03 +0600

    font added

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -12,9 +12,9 @@ function Inputs() {
             <UilSearch size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
               <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
           </div>
-          <div className="flex flex-row items-center justify-center w-1/4 space-x-2">
+          <div className="flex flex-row items-center justify-center w-1/4 space-x-1">
             <button name='matric' className="text-xl text-white font-light">°C</button>
-            <p>|</p>
+            <p className="text-white text-xl">|</p>
             <button name='matric' className="text-xl text-white font-light">°F</button>
           </div>
       </div>
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return (
+    <div>
+        <div className="flex items-center justify-center my-6">
+              <p className="text-white font-extralight text-xl">Tuesday, 31 May 2022 | Local Time: 12:46 PM</p>
+        </div>
+          <div className="flex items-center justify-center my-3">
+            <p className="text-white font-medium text-3xl">Barlin,DE</p>
+        </div>
+    </div>
+  )
+}
+
+export default TimeAndLocation
