e79bbf199a1a5f538856c899a5599f86b18c6f3a
Syed Nur.A.Rabbi Jim <syednur.jim@gmail.com>
2023-07-13 16:48:25 +0600

    open api call and luxon installed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecast from './components/Forecast';
+import getFormattedWeatherData from './services/weatherServices';
 function App() {
+  const fetchWeather = async () =>{
+    const data = await getFormattedWeatherData({q: "london"})
+    console.log(data);
+  }
+  fetchWeather();
   return (
     <div className= "mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButton/>
