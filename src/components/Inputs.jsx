71e4f5b3245ee015e69dd2bacfa14660b8226ed4
Syed Nur.A.Rabbi Jim <syednur.jim@gmail.com>
2023-07-13 19:51:52 +0600

    search option done

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
-function Inputs() {
+function Inputs({units, setUnits, setQuery}) {
+  const [city, setCity] = useState("");
+
+  const handlerSearchClick = () =>{
+    if(city !== '') setQuery({q: city})
+    setCity('');
+  }
   return (
       <div className="flex flex-row justify-center my-6">
           <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
             <input 
+            value={city}
+            onChange={ e => setCity(e.currentTarget.value)}
             type="text"
             placeholder='Search for city...'
                   className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase transition ease-out hover:scale-105"
             />
-            <UilSearch size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
-              <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
+            <UilSearch size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" onClick={handlerSearchClick} />
+            <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
           </div>
           <div className="flex flex-row items-center justify-center w-1/4 space-x-1">
             <button name='matric' className="text-xl text-white font-light">°C</button>
--- src/components/TopButton.jsx
+++ src/components/TopButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function TopButton() {
+function TopButton({setQuery}) {
     const cities =[
         {
             id: 1,
@@ -27,7 +27,7 @@ function TopButton() {
       <div className="flex items-center justify-around my-6">
         {
             cities.map(city=>(
-                <button key={city.id} className="text-white text-lg font-medium">{city.title}</button>
+                <button onClick={()=>setQuery({q: city.title})} key={city.id} className="text-white text-lg font-medium">{city.title}</button>
             ))
         }
     </div>
