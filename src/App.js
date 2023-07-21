9d2dfa40c42d29cd3d4644f929235151c7c9ca9f
s.zim <syednur.jim@gmail.com>
2023-07-21 06:50:29 +0600

    clear warning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButton from './components/TopButton';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
